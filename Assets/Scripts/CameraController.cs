54d3e71a774228c64962ba1f1da24aab4deaa9f9
Connor McCauley <cdmc13@outlook.com>
2019-04-02 16:49:44 -0400

    Fix to camera control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
         if (player.transform.localScale.x > 0f)
         {
             playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
