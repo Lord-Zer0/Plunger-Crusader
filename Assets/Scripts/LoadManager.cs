9e96e5e90bdf0524162be1b28fedbdaff62444a5
Connor McCauley <cdmc13@outlook.com>
2019-04-02 17:11:37 -0400

    added resources for pause menu and scene loader

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex) {
+		// Load a Scene with SceneManager
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
